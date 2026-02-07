1622f79082c5d42a8e6ff7ff4040ddd8638c10d3
JonJagger <jon@kosli.com>
2026-02-07 08:40:33 +0000

    Upgrade to Ruby 4.0.1, Cucumber 10.2.0, change default branch to main

--- check_version.sh
+++ check_version.sh
@@ -1,11 +1,13 @@
-#!/bin/bash -Eeu
+#!/usr/bin/env bash
+set -Eeu
+
 readonly MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 readonly REGEX="image_name\": \"(.*)\""
 readonly JSON=`cat ${MY_DIR}/docker/image_name.json`
 [[ ${JSON} =~ ${REGEX} ]]
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
-readonly EXPECTED="9.2.1"
+readonly EXPECTED="10.2.0"
 readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} sh -c 'cucumber --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
